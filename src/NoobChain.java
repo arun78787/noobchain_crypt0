25047dd44df2d28ac99f032bd2e4c6092f904046
arun78787 <arun9991421407yadav@gmail.com>
2025-01-01 20:32:57 +0530

    Initial commit

---
+++ src/NoobChain.java
@@ -0,0 +1,12 @@
+public class NoobChain{
+    public static void main(String[] args){
+        Block genesisBlock = new Block("hi im the first block", "0");
+        System.out.println("Hash for block 1: " + genesisBlock.hash);
+
+        Block secondBlock = new Block("yo im the second block", genesisBlock.hash);
+        System.out.println("Hash for block 2 : " + secondBlock.hash);
+        
+        Block thirdBlock = new Block("hey im the third block",secondBlock.hash);
+        System.out.println("Hash for block 3 : " + thirdBlock.hash);
+    }
+}
